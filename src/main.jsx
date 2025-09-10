3000f8f25f803052a47299958ea0585d3b6d06ff
frangersal <frangersal@outlook.com>
2025-09-09 21:36:51 -0600

    Eliminar estilos CSS obsoletos y agregar nuevos estilos para la calculadora utilizando neumorfismo y glassmorphism.

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+// import './default.css'
+// import './glasmorfism.css'
+import './neumorphism.css'
+// import './neobrutasilm.css'
+// import './y2k.css'
+// import './claymorphism.css'
 import {CalculadoraApp} from './calculadoraApp'
 
 createRoot(document.getElementById('root')).render(
